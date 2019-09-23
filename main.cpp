2873f5ff6f706a895f722ecdd7f7b83fce9e44e1
Sworce <Sworce>
2019-09-23 16:03:40 -0700

    Added input/output

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//Authors:
+//Authors: Tommy Luo
 #include<iostream>
 #include<string>
 
@@ -7,10 +7,17 @@ using namespace std;
 int main()
 {
   //declare variables
+  string fname = "";
+  string lname = "";
 
   //get user input
+  cout << "What is your first name?" << endl;
+  cin >> fname;
+  cout << "What is your last name?" << endl;
+  cin >> lname;
 
   //tell fortune
+  
 
   return 0;
 }
