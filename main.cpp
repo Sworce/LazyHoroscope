06c281edf4e84bc8314ab0637da9fa38f5a1566e
Sworce <Sworce>
2019-09-23 16:15:12 -0700

    added tell fortune

--- main.cpp
+++ main.cpp
@@ -9,15 +9,21 @@ int main()
   //declare variables
   string fname = "";
   string lname = "";
+  int lucky;
 
   //get user input
   cout << "What is your first name?" << endl;
   cin >> fname;
   cout << "What is your last name?" << endl;
   cin >> lname;
-
+ 
   //tell fortune
-  
+  cout << "Welcome, " << fname.at(0) << "." << lname.at(0) << "., " 
+  << "here is your fortune..." << endl;
+
+  lucky = fname.length();
+
+  cout << "your lucky number is " << lucky << endl;
 
   return 0;
 }
